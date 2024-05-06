7c24e1672e7125f7c4b39af1b7eff90ab64d72d4
AmineEl59 <120328924+AmineEl59@users.noreply.github.com>
2024-05-06 15:49:15 +0200

    Update gen.pdf.inc.php
    Correction bug génération pdf intervention

--- site/TCPDF/gen.pdf.inc.php
+++ site/TCPDF/gen.pdf.inc.php
@@ -1,17 +1,21 @@
 <?php
-// Initialiser une variable pour suivre si la génération du PDF a réussi ou échoué
-$pdf_generated = false;
-
 // Vérifiez si le formulaire a été soumis et si le bouton "Générer PDF" a été cliqué
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['generate_pdf'])) {
-    // Récupérer l'ID technicien sélectionné depuis le formulaire
-    $idTech = $_POST['idTech'];
-    $selectedDate = !empty($_POST['selectedDate']) ? $_POST['selectedDate'] : null;
+    // Récupérez l'ID de l'intervention sélectionnée
+    $selected_id = $_POST['generate_pdf'];
 
+    // Trouvez l'index de l'ID sélectionné dans le tableau des ID d'intervention
+    $index = array_search($selected_id, $_POST['idInter']);
+
+    // Récupérez les détails de l'intervention correspondante
+    $idInter = $_POST['idInter'][$index];
+    $clientInter = $_POST['clientInter'][$index];
+    $dateInter = $_POST['dateInter'][$index];
+    $dateFinInter = $_POST['dateFinInter'][$index];
+    $commentInter = $_POST['commentInter'][$index];
+    $etat = $_POST['etat'][$index];
+    
     try {
-        // Récupérer les informations d'intervention en fonction de l'ID du technicien et de la date sélectionnée (si elle est spécifiée)
-        $interventions = infosIntervention($idTech, $selectedDate);
-
         // Inclure le fichier principal de TCPDF
         require_once('tcpdf.php');
 
@@ -25,16 +29,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['generate_pdf'])) {
         $pdf->SetFont('helvetica', '', 12);
 
         // Écrire les informations de l'intervention dans le PDF
-        foreach($interventions as $intervention) {
-            $pdf->Cell(0, 10, 'ID Intervention: ' . $intervention['idInter'], 0, 1);
-            $pdf->Cell(0, 10, 'Client: ' . $intervention['clientInter'], 0, 1);
-            $pdf->Cell(0, 10, 'Date Intervention: ' . $intervention['dateInter'], 0, 1);
-            $pdf->Cell(0, 10, 'Date Fin Intervention: ' . $intervention['dateFinInter'], 0, 1);
-            $pdf->Cell(0, 10, 'Commentaire: ' . $intervention['commentInter'], 0, 1);
-            $pdf->Cell(0, 10, 'ID Technicien: ' . $intervention['idTech'], 0, 1);
-            $pdf->Cell(0, 10, 'État: ' . $intervention['etat'], 0, 1);
-            $pdf->Ln(); // Ajouter un saut de ligne entre les interventions
-        }
+        $pdf->Cell(0, 10, 'ID Intervention: ' . $idInter, 0, 1);
+        $pdf->Cell(0, 10, 'Client: ' . $clientInter, 0, 1);
+        $pdf->Cell(0, 10, 'Date Intervention: ' . $dateInter, 0, 1);
+        $pdf->Cell(0, 10, 'Date Fin Intervention: ' . $dateFinInter, 0, 1);
+        $pdf->Cell(0, 10, 'Commentaire: ' . $commentInter, 0, 1);
+        $pdf->Cell(0, 10, 'État: ' . $etat, 0, 1);
 
         // Nom du fichier de sortie
         $file_name = '/PHP/cashcash/pdf/interventions_' . date('YmdHis') . '.pdf';
